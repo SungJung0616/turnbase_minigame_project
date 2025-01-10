bdde0f07014d534dec14a2695e5a420961bb73da
SungJung <sungung0616@gmail.com>
2025-01-09 16:41:21 -0800

    Initail commit for minigame project

---
+++ C#_DailyStudy/Characters/Character.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__DailyStudy.Characters
+{
+    internal class Character
+    {
+        public string Name { get; set; }
+        public int HP { get; set; }
+        public int MP { get; set; }
+        public int Attack { get; set; }
+        public int MagicAttack { get; set; }
+        public int Defense { get; set; }
+
+        public virtual void ShowStats()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"HP: {HP}, Attack: {Attack}, MagicAttack: {MagicAttack} ,Defense: {Defense}");
+        }
+
+        public virtual void AttackTarget(Character target)
+        {
+            int damage = Attack - target.Defense;
+            if (damage < 0) damage = 0;
+
+            target.HP -= damage;
+            Console.WriteLine($"{Name} attacks {target.Name} for {damage} damage!");
+        }
+
+        public virtual void MagicAttackTarget(Character target)
+        {
+            if (MP < 10)
+            {
+                Console.WriteLine($"{Name} does not have enough MP to cast a spell!");
+                return;
+            }
+            MP -= 10;
+            int damage = MagicAttack;
+            if (damage < 0) damage = 0;
+            target.HP -= damage;
+            Console.WriteLine($"{Name} attacks {target.Name} with magic for {damage} damage!");
+        }
+
+
+    }
+}
---
+++ C#_DailyStudy/Characters/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__DailyStudy.Characters
+{
+    internal class Player : Character
+    {
+        public Player(string name)
+        {
+            Name = name;
+            HP = 100;
+            Attack = 20;
+            Defense = 10;
+        }
+    }
+}
